060d46618fb790b35ab97237a29864cc9fb1d677
AnushkaIsuranga <isuranga880@hotmail.com>
2025-03-24 09:09:14 +0530

    Update SensorDashboard.tsx

--- Web/src/components/home/SensorDashboard.tsx
+++ Web/src/components/home/SensorDashboard.tsx
@@ -58,7 +58,7 @@ const SensorDashboard: React.FC = () => {
         <p className="text-gray-700 mb-4">Measures Temperature and Humidity</p>
         <DHT22 />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {Object.entries(sensorData).map(([gas, value]) => (
           <div key={gas} className="bg-white shadow-md rounded-lg p-4 border border-gray-200">
             <h2 className="text-xl font-semibold text-gray-800">{gas}</h2>
